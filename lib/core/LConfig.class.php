9d85d03f74889443339450c724386e2020bb04d8
mbcraft <info@mbcraft.it>
2019-03-14 11:33:06 +0100

    Fixed and added internal config

--- lib/core/LConfig.class.php
+++ lib/core/LConfig.class.php
@@ -1,50 +1,74 @@
 <?php
 
 class LConfig {
-    
+
     use LReadHashMap;
-    
+
     private static $data = [];
     private static $init_called = false;
     private static $php_config_found = false;
     private static $json_config_found = false;
-    
+
     public static function phpConfigFound() {
         return self::$php_config_found;
     }
-    
+
     public static function jsonConfigFound() {
         return self::$json_config_found;
     }
-    
+
     public static function saveServerVar($var_name) {
         if (!isset(self::$data[$var_name])) {
             self::$data[$var_name] = $_SERVER[$var_name];
-            LOutput::framework_debug('Server var '.$var_name.' persisted into configuration ...');
+            LOutput::framework_debug('Server var ' . $var_name . ' persisted into configuration ...');
         }
     }
-        
+
     public static function init() {
-        
+
         if (self::$init_called) {
             throw new \Exception('Error : LConfig::init() called more than one time ...');
         }
         self::$init_called = true;
-        
+
+        // loading internal config ...
+
+        $path_parts = [];
+        $path_parts[] = 'config';
+        $path_parts[] = 'internal';
+        $path_parts[] = 'framework.json';
+
+        $internal_config_file_path = $_SERVER['PROJECT_DIR'] . implode('/', $path_parts);
+        if (!is_file($internal_config_file_path)) {
+            LOutput::error_message("Internal config not found : " . $internal_config_file_path);
+            exit(1);
+        }
+
+        try {
+            $internal_json_config = json_decode(file_get_contents($internal_config_file_path), true);
+            if (empty($internal_json_config))
+                throw new \Exception("Empty internal config found or error in json decoding ...");
+        } catch (\Exception $ex) {
+            LOutput::error_message("Errore nella lettura del file di configurazione interna " . $internal_config_file_path . " ...");
+            Loutput::exception($ex);
+            exit(1);
+        }
+
         // loading config ...
 
+        $path_parts = [];
         $path_parts[] = 'config';
         $path_parts[] = 'hostnames';
         $path_parts[] = $_SERVER['HOSTNAME'];
 
-        $config_dir_path = $_SERVER['PROJECT_DIR'].implode('/', $path_parts) . '/';
+        $config_dir_path = $_SERVER['PROJECT_DIR'] . implode('/', $path_parts) . '/';
 
         if (!is_dir($config_dir_path)) {
             LOutput::error_message("Config dir not found : " . $config_dir_path);
             exit(1);
         } else {
             // config dir found
-            LOutput::framework_debug("Config dir found : ".$config_dir_path);    
+            LOutput::framework_debug("Config dir found : " . $config_dir_path);
         }
 
         if (is_file($config_dir_path . 'config.php')) {
@@ -58,8 +82,8 @@ class LConfig {
         if (is_file($config_dir_path . 'config.json')) {
             self::$json_config_found = true;
             try {
-                $json_content = json_decode(file_get_contents($config_dir_path . 'config.json'), true);
-                if (empty($json_content))
+                $json_config = json_decode(file_get_contents($config_dir_path . 'config.json'), true);
+                if (empty($json_config))
                     throw new \Exception("Empty config found or error in json decoding ...");
             } catch (\Exception $ex) {
                 LOutput::error_message("Errore nella lettura del file di configurazione " . $config_dir_path . "config.json ...");
@@ -67,20 +91,22 @@ class LConfig {
                 exit(1);
             }
 
-            $all_config = array_replace_recursive($json_content,$php_config);
+            $all_config = array_replace_recursive($internal_json_config,$php_config);
             
-            self::$data = array_replace_recursive($all_config,self::$data);
-            
+            $all_config = array_replace_recursive($all_config, $json_config);
+
+            self::$data = array_replace_recursive($all_config, self::$data);
+
             // config loaded
             $message = "Config loaded : ";
             if (self::phpConfigFound())
-                $message .= '/config/hostnames/' .  $_SERVER['HOSTNAME'] . '/config.php';
+                $message .= '/config/hostnames/' . $_SERVER['HOSTNAME'] . '/config.php';
             if (self::phpConfigFound() && self::jsonConfigFound())
                 $message .= ' + ';
             if (self::jsonConfigFound())
-                $message .= '/config/hostnames/' .  $_SERVER['HOSTNAME'] . '/config.json';
+                $message .= '/config/hostnames/' . $_SERVER['HOSTNAME'] . '/config.json';
             Loutput::framework_debug($message);
-        } 
+        }
     }
-    
+
 }

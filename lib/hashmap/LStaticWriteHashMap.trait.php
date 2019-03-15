ebf42c9f9048dafe7f3f4f564bb5c8641d3a0a83
mbcraft <info@mbcraft.it>
2019-03-15 18:29:31 +0100

    More hash traits

---
+++ lib/hashmap/LStaticWriteHashMap.trait.php
@@ -0,0 +1,134 @@
+<?php
+
+trait LStaticWriteHashMap {
+    
+    /*
+     * Imposta un valore. L'ultima parte del path diventa la chiave.
+     * Se il valore è un Tree viene creato un link.
+     * Esempio :
+     * 
+     * path : /html/head/title
+     * value = "Benvenuti nel sito XYZ!!"
+     */
+    function set($path,$value)
+    {
+        $path_used = self::all_but_last_path_tokens($path);
+        
+        $current_node = &self::$data;
+        
+        foreach ($path_used as $p)
+        {            
+            if (!isset($current_node[$p]))
+                $current_node[$p] = array();
+
+            $current_node = &$current_node[$p];
+            
+        }
+        
+        if (is_object($value) && in_array('LStaticReadHashMap',class_uses(get_class($value))))
+            $current_node[self::last_path_token($path)] = $value::get("/");
+        else
+            $current_node[self::last_path_token($path)] = self::normalize_data($value);
+    }
+    
+    /*
+     * Aggiunge un valore all'array nella posizione corrente.
+     * Se il valore è un albero viene creato un link.
+     * Esempio :
+     * 
+     * path : /html/head/keywords
+     * value : ravenna
+     * 
+     * Viene aggiunta "ravenna" alle keywords. Keywords deve essere un array.
+     * 
+     * 
+     */
+    function add($path,$value)
+    {
+        $path_parts = self::path_tokens($path);
+        
+        $current_node = &self::$data;
+        
+        foreach ($path_parts as $p)
+        {
+            if (!isset($current_node[$p]))
+                $current_node[$p] = array();
+            $current_node = &$current_node[$p];
+        }
+        
+        if (is_object($value) && in_array('LStaticReadHashMap',class_uses(get_class($value))))
+            
+            $current_node[] = $value::get("/");
+        else
+            $current_node[] = $value;
+    }
+    
+    /*
+     * Effettua il merge di un'array di valori all'interno di un'altro array.
+     * La differenza rispetto ad add sta nel fondere i due array.
+     * Da usare se non si vogliono aggiungere i valori ad un array.
+     */
+    function merge($path,$value)
+    {
+        $real_value = self::normalize_data($value);
+
+        if (!is_array($real_value)) throw new InvalidParameterException("Il parametro passato non e' un array!!");
+
+        $path_parts = self::path_tokens($path);
+        
+        $current_node = &self::$data;
+        
+        foreach ($path_parts as $p)
+        {
+            if (!isset($current_node[$p]))
+                $current_node[$p] = array();
+            $current_node = &$current_node[$p];
+        }
+        
+        $current_node = array_merge($current_node,$real_value);
+    }
+    
+    /*
+     * Rimuove le chiavi trovate nel path specificato.
+     */
+    function purge($path,$keys)
+    {
+        $path_parts = self::path_tokens($path);
+        
+        $current_node = &self::$data;
+        
+        foreach ($path_parts as $p)
+        {
+            if (!isset($current_node[$p]))
+                $current_node[$p] = array();
+            $current_node = &$current_node[$p];
+        }
+        
+        $current_node = array_diff($current_node,$keys);
+    }
+    
+    function remove($path)
+    {
+        if (!self::is_set($path)) return;
+        else
+        {
+            $path_parts = self::all_but_last_path_tokens($path);
+        
+            $current_node = &self::$data;
+            foreach ($path_parts as $p)
+            {
+                $current_node = &$current_node[$p];
+            }
+            unset($current_node[self::last_path_token($path)]);
+        
+        }
+    }
+    
+    /**
+     * Svuota completamente la struttura dati.
+     */
+    public static function clear()
+    {
+        self::$data = array();
+    } 
+}

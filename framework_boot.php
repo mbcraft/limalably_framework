fdc02c54aa0cf13c016eb476f2dc89112a24cbf4
mbcraft <info@mbcraft.it>
2019-03-20 11:40:58 +0100

    Requires for db logging

--- framework_boot.php
+++ framework_boot.php
@@ -19,10 +19,17 @@ require_once ('lib/utils/LEnvironmentUtils.class.php');
 
 //logging
 require_once ('lib/logging/LILogger.interface.php');
-require_once ('lib/logging/LFileLogWriter.class.php');
-require_once ('lib/logging/LDistinctFileLog.class.php');
-require_once ('lib/logging/LTogetherFileLog.class.php');
+require_once ('lib/logging/LILogWriter.interface.php');
 require_once ('lib/logging/LLog.class.php');
+
+require_once ('lib/logging/file/LFileLogWriter.class.php');
+require_once ('lib/logging/file/LDistinctFileLog.class.php');
+require_once ('lib/logging/file/LTogetherFileLog.class.php');
+
+require_once ('lib/logging/db/LMysqlLogWriter.class.php');
+require_once ('lib/logging/db/LDbLog.class.php');
+
+
 
 //... misc others
 //routing

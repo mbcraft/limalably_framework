3ecfa3dfaeacbfbe4a1fc33a9486bf6af175c0d4
mbcraft <info@mbcraft.it>
2019-03-29 16:55:48 +0100

    Preparazione classi validazione parametri : condition

---
+++ tests/urlmap/LConditionTest.class.php
@@ -0,0 +1,55 @@
+<?php
+
+class LConditionTest extends LTestCase {
+    
+    function testCondition1() {
+        
+        $cond = array("a" => "a");
+        
+        $c = new LCondition($cond);
+        
+        $this->assertTrue($c->evaluate(),"La condizione non funziona!");
+        
+    }
+    
+    function testCondition2() {
+        
+        $cond = array("a" => [2,"a"]);
+        
+        $c = new LCondition($cond);
+        
+        $this->assertTrue($c->evaluate(),"La condizione non funziona!");
+        
+    }
+    
+    function testCondition3() {
+        
+        $cond = array("a" => [2,"a"],"b" => ["a","c"]);
+        
+        $c = new LCondition($cond);
+        
+        $this->assertFalse($c->evaluate(),"La condizione non funziona!");
+        
+    }
+    
+    function testCondition4() {
+        
+        $cond = true;
+        
+        $c = new LCondition($cond);
+        
+        $this->assertTrue($c->evaluate(),"La condizione non funziona!");
+        
+    }
+    
+    function testCondition5() {
+        
+        $cond = false;
+        
+        $c = new LCondition($cond);
+        
+        $this->assertFalse($c->evaluate(),"La condizione non funziona!");
+        
+    }
+    
+}

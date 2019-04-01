9679785368f19b875674b699dfd3da08cabce838
mbcraft <info@mbcraft.it>
2019-04-01 15:13:13 +0200

    Validation group

---
+++ lib/urlmap/validation/LParameterGroupValidator.class.php
@@ -0,0 +1,36 @@
+<?php
+
+class LParameterGroupValidator {
+    
+    private $treeview_params;
+    private $validation_list;
+    
+    function __construct($treeview_params,$validation_list) {
+        
+        $this->treeview_params = $treeview_params;
+        $this->validation_list = $validation_list;
+        
+    }
+    
+    function validate() {
+        
+        $final_result = [];
+        
+        foreach ($this->validation_list as $name => $params) {
+            
+            $is_set = $this->treeview_params->is_set($name);
+            if ($is_set) $value = $this->treeview_params->get($name);
+            else $value = null;
+            
+            $validator = new LParameterValidator($name,$is_set,$value,$params);
+            
+            $final_result = array_merge($final_result,$validator->validate());
+            
+            $this->treeview_params->set($name,$validator->getNormalizedValue());
+        }
+        
+        return $final_result;
+        
+    }
+    
+}

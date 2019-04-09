fe3e9faf99ea71a8b8115bd46a36e77c39e2af82
mbcraft <info@mbcraft.it>
2019-04-09 15:11:22 +0200

    Fixed patterns

--- lib/core/LClassLoader.class.php
+++ lib/core/LClassLoader.class.php
@@ -21,10 +21,10 @@ function project_require($filename_relative_path) {
 class LClassLoader {
       
     //patterns
-    const PATTERN_FIND_NAMESPACES = "/(\s)*namespace(\s)+(?<namespace>[a-zA-Z_0-9\\\\]+)[;{ \n]+/i";
-    const PATTERN_FIND_CLASSES = "/(\s)*(abstract)?(final)?(\s)*class[ ]+(?<class>[a-zA-Z_0-9]+)[{ \n]+/i";
-    const PATTERN_FIND_TRAITS = "/(\s)*trait(\s)+(?<trait>[a-zA-Z_0-9]+)[{ \n]+/i";
-    const PATTERN_FIND_INTERFACES = "/(\s)*interface(\s)+(?<interface>[a-zA-Z_0-9]+)[{ \n]+/i";
+    const PATTERN_FIND_NAMESPACES = "/\n(\s)*namespace(\s)+(?<namespace>[a-zA-Z_0-9\\\\]+)[;{ \n]+/i";
+    const PATTERN_FIND_CLASSES = "/\n(\s)*(abstract)?(final)?(\s)*class(\s)+(?<class>[a-zA-Z_0-9]+)[{ \n]+/i";
+    const PATTERN_FIND_TRAITS = "/\n(\s)*trait(\s)+(?<trait>[a-zA-Z_0-9]+)[{ \n]+/i";
+    const PATTERN_FIND_INTERFACES = "/\n(\s)*interface(\s)+(?<interface>[a-zA-Z_0-9]+)[{ \n]+/i";
     
     private static $class_map = [];
     

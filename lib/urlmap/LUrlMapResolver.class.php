39410a0fcc4d03a945a3f619c6f22bae3b7d3ea8
mbcraft <info@mbcraft.it>
2019-05-02 15:04:22 +0200

    Fixed folder route

--- lib/urlmap/LUrlMapResolver.class.php
+++ lib/urlmap/LUrlMapResolver.class.php
@@ -279,7 +279,7 @@ class LUrlMapResolver {
     
     public function resolveUrlMap(string $route, int $search_flags = self::FLAGS_SEARCH_ALL) {
         
-        if (!$route) $route = '_folder';
+        if (!$route) $route = $this->folder_route;
         
         if (!$this->isInitialized()) $this->initWithDefaults ();
         

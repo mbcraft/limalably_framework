cfab214cb8cf26b42d3ab24e94aea1d323c6a403
Marco Bagnaresi <info@mbcraft.it>
2022-08-17 18:19:28 +0200

    Added mysql multi query class

---
+++ lib/db/query/mysql/LMysqlQueryList.class.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @author MBCRAFT di Marco Bagnaresi - mail : info@mbcraft.it
+ * 
+ *  
+ */
+
+
+class LMysqlMultiQuery {
+	
+
+	private $query_list;
+
+	function __construct($query_list_or_file) {
+
+		if ($query_list_or_file instanceof LFile) {
+			$this->query_list = $query_list_or_file->getContent();
+			return;
+		} 
+		if (is_string($query_list_or_file))
+		{
+			$this->query_list = $query_list_or_file;
+			return;
+		}
+
+		throw new \Exception("Unrecognized parameter for mysql query list. Actually only string or LFile instances are allowed");
+
+	}
+
+	function go($connection) {
+
+		if (!$connection) throw new \Exception("Connection is not set!");
+
+		if (!$connection->isOpen()) $connection->open();
+
+		$connection_handle = $connection->getHandle();
+
+		mysqli_multi_query($connection_handle,$this->query_list);
+		
+	}
+
+}

0f7a42295682049973148f886172d81df5af08ff
Marco Bagnaresi <info@mbcraft.it>
2022-08-24 12:35:48 +0200

    Added comment headers in some files

--- lib/db/query/mysql/LMysqlAbstractInsertOrReplaceStatement.class.php
+++ lib/db/query/mysql/LMysqlAbstractInsertOrReplaceStatement.class.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * @author MBCRAFT di Marco Bagnaresi - mail : info@mbcraft.it
+ * 
+ *  
+ */
+
+/*
+
+Thanks to www.mysqltutorial.org for its documentation.
+
+*/
+
 
 abstract class LMysqlAbstractInsertOrReplaceStatement extends LMysqlAbstractQuery {
 	
--- lib/db/query/mysql/LMysqlCreateTableStatement.class.php
+++ lib/db/query/mysql/LMysqlCreateTableStatement.class.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * @author MBCRAFT di Marco Bagnaresi - mail : info@mbcraft.it
+ * 
+ *  
+ */
+
+/*
+
+Thanks to www.mysqltutorial.org for its documentation.
+
+*/
+
 
 class LMysqlCreateTableStatement extends LMysqlAbstractQuery {
 	
